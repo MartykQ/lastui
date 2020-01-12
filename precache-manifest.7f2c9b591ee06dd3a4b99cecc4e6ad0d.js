b09e8704c60930bc76f15f18561cfa2331f7a4bf
MartykQ <fmartyka16@gmail.com>
2020-01-12 11:17:24 +0100

    Updates

---
+++ precache-manifest.7f2c9b591ee06dd3a4b99cecc4e6ad0d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d20af6ef86d92a3007b362fde5eadc8",
+    "url": "/lastui/index.html"
+  },
+  {
+    "revision": "e102d8c035bde37c2471",
+    "url": "/lastui/static/css/2.9a44717d.chunk.css"
+  },
+  {
+    "revision": "626f28f006aadb26e514",
+    "url": "/lastui/static/css/main.772e30cf.chunk.css"
+  },
+  {
+    "revision": "e102d8c035bde37c2471",
+    "url": "/lastui/static/js/2.34c91f68.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/lastui/static/js/2.34c91f68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "626f28f006aadb26e514",
+    "url": "/lastui/static/js/main.5830c797.chunk.js"
+  },
+  {
+    "revision": "8fdeda937066085a6dd2",
+    "url": "/lastui/static/js/runtime-main.c140adfc.js"
+  },
+  {
+    "revision": "afeab39d76aa198a058214c7da2be357",
+    "url": "/lastui/static/media/avatar.afeab39d.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lastui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/lastui/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/lastui/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/lastui/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/lastui/static/media/notification.c0d3c94c.eot"
+  }
+]);
